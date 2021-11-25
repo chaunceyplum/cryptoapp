47c18e1bdee9e1a48c9cf40a3042d89e3792d8af
Chauncey <Chaunceyplum@gmail.com>
2021-11-25 14:08:05 -0500

    fixed responsiveness

--- src/components/DisplayComponent.js
+++ src/components/DisplayComponent.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card, Col, Row } from 'react-bootstrap'
 import { CardBody, CardHeader, CardSubtitle, CardTitle } from 'reactstrap'
 
@@ -6,20 +6,43 @@ import { CardBody, CardHeader, CardSubtitle, CardTitle } from 'reactstrap'
 
 const DisplayComponent = (props) => {
     
+    const [search, setSearch] = useState('')
+
+
+
+
     return (
         
         <div>
-            
+             <Row className="d-flex justify-content-center spacer">
+                    <Col xs={1}/>
+                    <Col xs={10} className="mx-auto ">
+                        <input type="text" onChange={event =>setSearch(event.target.value)} className="" placeholder="Search by name"/>
+                    </Col>
+                    <Col xs={1} />
+                </Row>
+
+
+                
             
         
             {
+
+                props.data.data.filter((cur) => {
+                    if (search =="") {
+                        return cur
+                        
+                    } else if (cur.name.toLowerCase().includes(search.toLowerCase()) ||cur.symbol.toLowerCase().includes(search.toLowerCase())) {
+                        return cur
+                    } 
+                })
                 
-                props.data.data.map((curr, index) => {
+                .map((curr, index) => {
                     return(
                         
                         
-                            <div  >
-                                <Card key = {index}  className='cardy'>
+                            <div >
+                                <Card key = {index}  className='cardy' >
                                     <CardHeader className="cardHeader">
                                         <h1 className="text-center">
                                             {
--- src/components/Homescreen.js
+++ src/components/Homescreen.js
@@ -7,22 +7,11 @@ const Homescreen = () => {
     
     const [data, setData] = useState([])
     const [loaded, setLoaded] =useState(false)
-    const [search , setSearch] = useState('')
 
 
     useEffect(() => {
         try {
             fetch('https://api.coincap.io/v2/assets'
-        //     ,
-        //  {
-        //     mode:'cors',
-        //     headers: {
-        //         'Content-Type' : 'application/json',
-        //         // 'Access-Control-Allow-Origin' : '*'
-        //     } 
- 
-
-        // }
         )
         .then(res => res.json())
         .then(res => {
@@ -42,11 +31,7 @@ const Homescreen = () => {
         
     },[])
 
-    // const handleChange = e => {
-    //     setSearch(e.target.value)
-    // }
-    // const searchRes = data.data.filter((currency) => currency.name.toLowerCase().includes(search.toLowerCase())
-    // )
+    
     
     return (
         <div>
@@ -62,92 +47,20 @@ const Homescreen = () => {
                     <p></p>
 
                 </Row>
-                <Row>
-                    {/* <form>
-                        <input type="text" placeholder="Search" onChange={handleChange} />
-                    </form> */}
-                </Row>
+                
                     <div  className="center ">
-                     {/* searchComponent */}
-                        {/* {
-                            searchRes.map(curr => {
-                                return(
-                                    <DisplayComponent data={data}/>
-                                )
-                            })
-                        } */}
-                        
-                        {/* display #1 */}
+                    
                         {
                             loaded === true ?
-                            <DisplayComponent data={data}   />
+                            <Col xs={11} sm={9} className="mx-auto">
+                                <DisplayComponent data={data}   />
+                            </Col>
                             :
                             <div>
                                 ...loading
                             </div>
                         }
-
-
-
-                        {/* display #2 */}
-                        {/* {
-                            data.data.map((curr, index) => {
-                                return(
-                                    
-                                    
-                                        <div  >
-                                            <Card key = {index}  className='cardy'>
-                                                <CardHeader className="cardHeader">
-                                                    <h1 className="text-center">
-                                                        {
-                                                            curr.name
-                                                        }
-                                                    </h1>
-                                                    <CardSubtitle className="text-center">
-                                                        <p>
-                                                            {
-                                                                curr.symbol
-                                                            }
-                                                        </p>
-                                                    </CardSubtitle>
-                                                </CardHeader>
-                                                <CardBody className="cardBody">
-                                                    <ul>
-                                                        <li className="text-center">
-                                                            {
-                                                                `United States Dollar:  ${curr.priceUsd}`
-                                                            }
-                                                        </li>
-                                                        <br />
-                                                        <li className="text-center">
-                                                            {
-                                                                `Market Cap:    ${curr.marketCapUsd}`
-                                                            }
-                                                        </li>
-                                                        <br />
-                                                        <li className="text-center">
-                                                            {
-                                                                `Volume:   ${curr.volumeUsd24Hr}`
-                                                            }
-                                                        </li>
-                                                        <br />
-                                                        <li className="text-center">
-                                                            {
-                                                                `Percent of Change/24hr: ${curr.changePercent24Hr}`
-                                                            }
-                                                        </li>
-                                                        
-                                                    </ul>
-            
-                                                </CardBody>
-                                            </Card>
-                                            <br />
-                                            <br />
-                                    
-                                        </div>
-                                )
-                            })
-                        } */}
+                        
                     </div>
                 
             </div>
